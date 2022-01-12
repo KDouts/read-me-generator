08c822fb711d7c0403fb814d8097046a88f3bfd9
Kurtis Doute <kcd257@yahoo.com>
2022-01-12 12:09:24 -0800

    added init function and writeToFile function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -15,10 +16,22 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+  err ? console.error(err): console.log('Your ReadMe has been successfully created :)')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then(answers => {
+      const userData = answers;
+      writeFile('projects.md', userData)
+      console.log(answers);
+    })
+}
 
 // Function call to initialize app
 init();

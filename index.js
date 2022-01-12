83feb0bbfb160cec96af5957470469fa2a1f101a
Kurtis Doute <kcd257@yahoo.com>
2022-01-12 12:00:36 -0800

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What is your propject about?',
+        name: 'bio',
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

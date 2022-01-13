90fb68551e79632b70537abb84a5c6d8e414d8ea
Kurtis Doute <kcd257@yahoo.com>
2022-01-12 18:02:05 -0800

    Updated functionality of the answers in the read me appearing into a new file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
 
 const questions = [
       {
@@ -46,14 +44,12 @@ const questions = [
       }     
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) =>
   err ? console.error(err): console.log('Your ReadMe has been successfully created :)')
   );
 }
 
-// TODO: Create a function to initialize app
 function init() {
   inquirer
     .prompt(questions)
